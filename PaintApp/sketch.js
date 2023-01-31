4da57800be648ff1953b967e90448578cc476cee
lfrick3 <lfrick3@lsu.edu>
2023-01-31 14:36:03 -0600

    Finished paint app functionality

--- PaintApp/sketch.js
+++ PaintApp/sketch.js
@@ -1,3 +1,5 @@
+let colorSet = ('black');
+
 function setup() {
   createCanvas(800, 600);
   background(185);
@@ -6,25 +8,59 @@ function setup() {
 function draw() {
   push();
   stroke(255, 255, 255);
-  fill(255, 0, 0, 255);
+  //red
+  fill('red');
   rect(0, 0, 50, 50);
-  fill(255, 125, 0, 255);
+  //orange
+  fill('orange');
   rect(0, 50, 50, 50);
-  fill(255, 255, 0, 255);
+  //yellow
+  fill('yellow');
   rect(0, 100, 50, 50);
-  fill(0, 255, 0, 255);
+  //green
+  fill('green');
   rect(0, 150, 50, 50);
-  fill(0, 255, 255, 255);
+  //cyan
+  fill('cyan');
   rect(0, 200, 50, 50);
-  fill(0, 0, 255, 255);
+  //blue
+  fill('blue');
   rect(0, 250, 50, 50);
-  fill(255, 0, 255, 255);
+  //magenta
+  fill('magenta');
   rect(0, 300, 50, 50);
-  fill(155, 100, 0, 255);
+  //brown
+  fill('brown');
   rect(0, 350, 50, 50);
-  fill(255, 255, 255, 255);
+  //white
+  fill('white');
   rect(0, 400, 50, 50);
-  fill(0, 0, 0, 255);
+  //black
+  fill('black');
   rect(0, 450, 50, 50);
+  pop();
+}
+
+function mousePressed() {
+  if (mouseX >= 0 && mouseX <= 50 &&
+      mouseY >= 0 && mouseY <= 500) {
+        if (mouseY <= 50) {colorSet = ('red')}
+        else if (mouseY > 50 && mouseY <= 100) {colorSet = ('orange')}
+        else if (mouseY > 100 && mouseY <= 150) {colorSet = ('yellow')}
+        else if (mouseY > 150 && mouseY <= 200) {colorSet = ('green')}
+        else if (mouseY > 200 && mouseY <= 250) {colorSet = ('cyan')}
+        else if (mouseY > 250 && mouseY <= 300) {colorSet = ('blue')}
+        else if (mouseY > 300 && mouseY <= 350) {colorSet = ('magenta')}
+        else if (mouseY > 350 && mouseY <= 400) {colorSet = ('brown')}
+        else if (mouseY > 400 && mouseY <= 450) {colorSet = ('white')}
+        else if (mouseY > 450 && mouseY <= 500) {colorSet = ('black')}
+      }
+}
+
+function mouseDragged() {
+  push();
+  stroke(colorSet);
+  strokeWeight(10);
+  line(mouseX, mouseY, pmouseX, pmouseY);
   pop();
 }
