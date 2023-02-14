f917fde0792a347c69bb7f86a901e6c08ec80667
lfrick3 <lfrick3@lsu.edu>
2023-02-14 13:51:48 -0600

    added y direction of movement

--- BugSquish/sketch.js
+++ BugSquish/sketch.js
@@ -1,10 +1,11 @@
 class Creature {
-  constructor (sprites, x, y) {
+  constructor (sprites, x, y, xdir, ydir) {
     this.frameCount = 0;
     this.sprites = sprites;
     this.dx = x;
     this.dy = y;
-    this.xDir = 1;
+    this.xDir = xdir;
+    this.yDir = ydir;
     this.sx = 0;
     this.sy = 0;
     this.sw = 80;
@@ -15,32 +16,35 @@ class Creature {
     this.currentWalkFrame = 0;
   }
 
-  walkX(val) {
+  walk(xval, yval) {
     this.u = this.currentWalkFrame % this.walkAnimationLength;
-    this.dx += (val * 1.5);
+    this.dx += (xval * 1.5);
+    this.dy += (yval * 1.5);
+    //x movement limits
     if (this.dx < (0 + this.sw / 2)) {
       this.dx = 0 + this.sw / 2;
+      this.xDir = -1 * this.xDir;
     } else if (this.dx > (800 - this.sw / 2)) {
       this.dx = 800 - this.sw / 2;
+      this.xDir = -1 * this.xDir;
     }
+    //y movement limits
+    if (this.dy < (0 + this.sh / 2)) {
+      this.dy = 0 + this.sh / 2;
+      this.yDir = -1 * this.yDir;
+    } else if (this.dy > (800 - this.sh / 2)) {
+      this.dy = 800 - this.sh / 2;
+      this.yDir = -1 * this.yDir;
+    }
+    //framerate of animation
     if (this.frameCount % 6 == 0) {
       this.currentWalkFrame ++;
     }
   }
 
-  //EDIT THIS OUT AS ITS A TARGET, NOT THE PC ANY MORE
   display() {
-    //walking
-    if (keyIsDown(LEFT_ARROW)) {
-      this.xDir = -1;
-      this.walkX(this.xDir);
-    } else if (keyIsDown(RIGHT_ARROW)) {
-      this.xDir = 1;
-      this.walkX(this.xDir);
-    } else {
-      this.u = 0;
-      this.currentWalkFrame = 0;
-    }
+    //start movement
+    this.walk(this.xDir, this.yDir);
 
     //character sprite
     push();
@@ -49,7 +53,7 @@ class Creature {
     image(this.sprites, 0, 0, 80, 80, this.u*this.sw, this.v*this.sh, this.sw, this.sh);
     pop();
 
-    //framerate
+    //frame counting
     if (this.frameCount > 60) {
       this.frameCount = 0;
     } else {
@@ -60,6 +64,8 @@ class Creature {
 
 let spritesheetBug;
 let bugs;
+let score;
+let timer;
 
 function preload() {
   spritesheetBug = loadImage("assets/Bug.png");
@@ -68,14 +74,31 @@ function preload() {
 function setup() {
   createCanvas(800, 800);
   imageMode(CENTER);
-  bugs = [new Creature(spritesheetBug, random(40, 760), random(40, 760)),
-          new Creature(spritesheetBug, random(40, 760), random(40, 760)),
-          new Creature(spritesheetBug, random(40, 760), random(40, 760)),
-          new Creature(spritesheetBug, random(40, 760), random(40, 760)),
-          new Creature(spritesheetBug, random(40, 760), random(40, 760))]
+  score = 0;
+  timer = 30;
+  bugs = [new Creature(spritesheetBug, random(40, 760), random(40, 760), xDirCalc(), yDirCalc()),
+          new Creature(spritesheetBug, random(40, 760), random(40, 760), xDirCalc(), yDirCalc()),
+          new Creature(spritesheetBug, random(40, 760), random(40, 760), xDirCalc(), yDirCalc()),
+          new Creature(spritesheetBug, random(40, 760), random(40, 760), xDirCalc(), yDirCalc()),
+          new Creature(spritesheetBug, random(40, 760), random(40, 760), xDirCalc(), yDirCalc())]
 }
 
 function draw() {
   background(180);
   bugs.display();
+}
+
+function xDirCalc() {
+  return Math.random() < 0.5 ? -1 : 1;
+}
+
+function yDirCalc() {
+  var num = Math.random();
+  if (num > 0.8) {
+    return 1;
+  } else if (num < 0.2) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
